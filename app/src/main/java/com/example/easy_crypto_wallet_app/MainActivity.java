3908415ad68b103a29298cfb18b416ec49a7c117
asaWasa <300401fdsa+->
2021-11-23 17:05:47 +0300

    first work version

--- app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
+++ app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -14,19 +21,38 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text_view;
+    private TextView text_view, input_address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text_view = findViewById(R.id.textView);
+        input_address = findViewById(R.id.input_address);
 
-        OkHttpClient client = new OkHttpClient();
-        String url = "https://0.0.0.0:11000/api/v1/get_ethereum_balance?address=0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c";
+    }
+
+    public void GetBalance(View view){
+        OkHttpClient.Builder builder = new OkHttpClient.Builder().followSslRedirects(false)
+                .followRedirects(false);
+        builder = configureToIgnoreCertificate(builder);
+        OkHttpClient client = builder.build();
+        // String url = "http://www.cbr.ru/scripts/XML_daily.asp";
+
+        String url = "https://10.0.2.2:11000/api/v1/get_ethereum_balance?address=" + input_address.getText().toString();
         Request req = new Request.Builder().url(url).build();
+
 
         client.newCall(req).enqueue(new Callback() {
             @Override
@@ -38,15 +64,59 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 if (response.isSuccessful()){
                     final String myResponse = response.body().string();
+                    JsonObject jsonObject = new JsonParser().parse(myResponse).getAsJsonObject();
 
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            text_view.setText(myResponse);
+                            text_view.setText(jsonObject.get("balance").getAsString());
+                            System.out.println(myResponse);
                         }
                     });
                 }
             }
         });
+
+    }
+
+    private static OkHttpClient.Builder configureToIgnoreCertificate(OkHttpClient.Builder builder) {
+        try {
+
+            /*Create a trust manager that does not validate certificate chains*/
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            /*Install the all-trusting trust manager*/
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            /*Create an ssl socket factory with our all-trusting manager*/
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+        }
+        return builder;
     }
 }
