ec1379c760f3359a2c766fe6ac52dc01116c3f97
Vlad Korovnikov <v.korovnikov@oom.ag>
2021-11-23 14:24:52 +0300

    http request example

--- app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
+++ app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.easy_crypto_wallet_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView text_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text_view = findViewById(R.id.textView);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://0.0.0.0:11000/api/v1/get_ethereum_balance?address=0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c";
+        Request req = new Request.Builder().url(url).build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    final String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            text_view.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
