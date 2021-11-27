84a59957f798126c2c0c3a36f4f812eb7d8aab00
asaWasa <300401fdsa+->
2021-11-28 02:15:27 +0300

    design first page

--- app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
+++ app/src/main/java/com/example/easy_crypto_wallet_app/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,91 +33,13 @@ import javax.net.ssl.X509TrustManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView text_view, input_address;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text_view = findViewById(R.id.textView);
-        input_address = findViewById(R.id.input_address);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
     }
 
-    public void GetBalance(View view){
-        OkHttpClient.Builder builder = new OkHttpClient.Builder().followSslRedirects(false)
-                .followRedirects(false);
-        builder = configureToIgnoreCertificate(builder);
-        OkHttpClient client = builder.build();
-        // String url = "http://www.cbr.ru/scripts/XML_daily.asp";
-
-        String url = "https://10.0.2.2:11000/api/v1/get_ethereum_balance?address=" + input_address.getText().toString();
-        Request req = new Request.Builder().url(url).build();
-
-
-        client.newCall(req).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()){
-                    final String myResponse = response.body().string();
-                    JsonObject jsonObject = new JsonParser().parse(myResponse).getAsJsonObject();
-
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            text_view.setText(jsonObject.get("balance").getAsString());
-                            System.out.println(myResponse);
-                        }
-                    });
-                }
-            }
-        });
-
-    }
-
-    private static OkHttpClient.Builder configureToIgnoreCertificate(OkHttpClient.Builder builder) {
-        try {
-
-            /*Create a trust manager that does not validate certificate chains*/
-            final TrustManager[] trustAllCerts = new TrustManager[] {
-                    new X509TrustManager() {
-                        @Override
-                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
-                                throws CertificateException {
-                        }
-
-                        @Override
-                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
-                                throws CertificateException {
-                        }
-
-                        @Override
-                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                            return new java.security.cert.X509Certificate[]{};
-                        }
-                    }
-            };
-
-            /*Install the all-trusting trust manager*/
-            final SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            /*Create an ssl socket factory with our all-trusting manager*/
-            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
-            builder.hostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        } catch (Exception e) {
-        }
-        return builder;
-    }
 }
